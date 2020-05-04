2974753e1c2e5738cb99ffe71209ccfc5da11a96
Alain D'Ettorre <alain.det@gmail.com>
2020-05-05 00:11:19 +0200

    Add styling to <ui-button>

--- src/app/shared/ui/components/button/button.component.ts
+++ src/app/shared/ui/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ui-button',
@@ -6,9 +6,34 @@ import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
   styleUrls: ['./button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
 
   @Input() size: 'small' | 'medium' | 'large' = 'medium';
   @Input() type: 'submit' | 'button' = 'button';
+  @Input() color: 'secondary' | 'primary' = 'secondary';
   @Input() disabled = false;
+  @Input() noMargin: string | boolean;
+  @Input() isFullWidth: string | boolean;
+
+  ngOnInit() {
+    this.processBooleanInputs([
+      'noMargin',
+      'isFullWidth',
+    ]);
+  }
+
+  private processBooleanInputs(keys: string[]): void {
+    for (const key of keys) {
+
+      // All missing keys are false
+      if (!this.hasOwnProperty(key)) {
+        this[key] = false;
+      }
+
+      // All strings, including '', are true. Boolean already work on their own
+      if (typeof this[key] === 'string') {
+        this[key] = this[key] || this[key] === '';
+      }
+    }
+  }
 }
