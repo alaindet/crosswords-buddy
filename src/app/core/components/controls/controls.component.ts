d59ffbff3f365d12a27aa63c056da70154da9611
Alain D'Ettorre <alain.det@gmail.com>
2020-05-05 02:58:43 +0200

    Add styling to <app-controls>

--- src/app/core/components/controls/controls.component.ts
+++ src/app/core/components/controls/controls.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 
+import { UiService } from 'src/app/core/services/ui.service';
+import { DefinitionsService } from 'src/app/core/services/definitions.service';
+import { Direction } from 'src/app/core/models/direction.enum';
+
 @Component({
   selector: 'app-controls',
   templateUrl: './controls.component.html',
   styleUrls: ['./controls.component.scss']
 })
 export class ControlsComponent {
+
+  Direction = Direction;
+
+  constructor(
+    public ui: UiService,
+    private definitionsService: DefinitionsService,
+  ) {}
+
+  onFilterClick(dir: Direction) {
+    this.definitionsService.setDirection(dir);
+  }
 
   onButtonClick(index: number) {
     console.log('onButtonClick', index);
